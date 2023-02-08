c8b9d6927826ed89c5307aac13372264da6d1c1e
manuelpagoada <mpagoada@gmail.com>
2023-02-08 10:58:13 -0800

    set up the driver file, commented everything that needs to be done

---
+++ driver.cpp
@@ -0,0 +1,32 @@
+//
+// Created by manuelpagoada on 2/8/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nameOfInputFile,nameOfOutPutFile, nameOfTestFile;
+
+    //prompt user for the name of the input file to use
+    cout << "Please enter the Name of the Input File: ";
+    cin >> nameOfInputFile;
+
+    //prompt user for the name of the output file to use
+    cout << "Please enter the Name of the Output File: ";
+    cin >> nameOfOutPutFile;
+
+    //prompt user for the name of the test run
+    cout << "Please enter the Name of the Test File: ";
+
+
+    //open the input file, reading line by line, and execute the instructions
+
+    //display the result to the console and write the result to the output file
+
+
+
+
+
+    return 0;
+}
