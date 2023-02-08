a4508cdbafe1bf92e436572f8052e42f193622bf
manuelpagoada <mpagoada@gmail.com>
2023-02-08 13:21:39 -0800

    delcared file stream variable, used user input, close file stream variable. Added notes

--- driver.cpp
+++ driver.cpp
@@ -3,10 +3,12 @@
 //
 
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 int main(){
-    string nameOfInputFile,nameOfOutPutFile, nameOfTestFile;
+    string nameOfInputFile,nameOfOutPutFile, nameOfTestRun;
 
     //prompt user for the name of the input file to use
     cout << "Please enter the Name of the Input File: ";
@@ -17,16 +19,32 @@ int main(){
     cin >> nameOfOutPutFile;
 
     //prompt user for the name of the test run
-    cout << "Please enter the Name of the Test File: ";
+    cout << "Please enter the Name of the Test Run: ";
 
 
     //open the input file, reading line by line, and execute the instructions
+
+    //declare variables
+    ifstream inputFile;
+
+    //Associate the file variable with the source file
+    inputFile.open(nameOfInputFile);
 
     //display the result to the console and write the result to the output file
 
 
 
+    /*  to check the file is open
+     *
+     *      if(inputFile.is_open()){}
+     *
+     *
+     * */
 
+
+
+    //close variables before ending program
+    inputFile.close();
 
     return 0;
 }
